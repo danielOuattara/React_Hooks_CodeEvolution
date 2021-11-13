a0752d2af7d3e1b52e8573761024cdaf2be4c3d3
daniel ouattara <daniel.ouattara@gmx.com>
2021-11-13 15:02:59 +0100

    19 simple useReducer

---
+++ 19_useReducer_Simple_State_and_Action/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CounterOne from './components/CounterOne';
+
+
+function App() {
+  return <CounterOne />
+}
+
+export default App;
+
+
+
---
+++ 19_useReducer_Simple_State_and_Action/src/components/CounterOne.js
@@ -0,0 +1,30 @@
+
+import {useReducer} from 'react';
+
+const initialState = 0;
+const reducer = (state, action) => {
+    switch(action) {
+        case 'INCREMENT' :
+            return  state + 1
+        case 'DECREMENT' :
+            return state - 1
+
+        case 'RESET' :
+            return initialState
+        default: 
+        return state;
+    }
+};
+
+function CounterOne() {
+    const [count, dispatch] = useReducer(reducer, initialState);
+    return (
+        <>
+            <h2>Count : { count }</h2>
+            <button onClick={() => dispatch('INCREMENT')}>Increment</button>
+            <button onClick={() => dispatch('DECREMENT')}>Decrement</button>
+            <button onClick={() => dispatch('RESET')}>Reset</button> 
+        </>
+    )
+}
+export default CounterOne;
